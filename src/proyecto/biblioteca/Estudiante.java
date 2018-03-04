2f7b34135c50afc199b283caf56bcff0b62cc374
Joseph Salas Solano <josephsalas1112@gmail.com>
2018-03-04 12:35:41 -0600

    Clases a utilizar

--- src/proyecto/biblioteca/Estudiante.java
+++ src/proyecto/biblioteca/Estudiante.java
@@ -1,37 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.biblioteca;
 
-/**
- *
- * @author Joseph Salas
- */
-public class Estudiante {
 
-    public Estudiante() {
+public class Estudiante {
+    private String Nombre;
+    private String Carnet;
+    private String Clave;
+    private int Morosidades = 0; //Comienza sin morosidades
+    private Libro[] Libros_Alquilados;
+    
+    //Constructor
+    public Estudiante(String nombre, String carnet, String clave) {
+        super();
+        Nombre = nombre;
+        Carnet = carnet;
+        Clave = clave;
     }
     
-        private String string;
-
-    /**
-     * Get the value of string
-     *
-     * @return the value of string
-     */
-    public String getString() {
-        return string;
+    //Setters y Getters
+    public String getNombre() {
+        return Nombre;
     }
-
-    /**
-     * Set the value of string
-     *
-     * @param string new value of string
-     */
-    public void setString(String string) {
-        this.string = string;
+    
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    
+    public String getCarnet() {
+        return Carnet;
+    }
+    
+    public void setCarnet(String carnet) {
+        Carnet = carnet;
+    }
+    
+    public String getClave() {
+        return Clave;
+    }
+    
+    public void setClave(String clave) {
+        Clave = clave;
+    }
+    
+    public int getMorosidades() {
+        return Morosidades;
+    }
+    
+    public void setMorosidades(int morosidades) {
+        Morosidades = morosidades;
+    }
+    
+    public Libro[] getLibros_Alquilados() {
+        return Libros_Alquilados;
+    }
+    
+    public void setLibros_Alquilados(Libro[] libros_Alquilados) {
+        Libros_Alquilados = libros_Alquilados;
     }
 
     
--- src/proyecto/biblioteca/Libro.java
+++ src/proyecto/biblioteca/Libro.java
@@ -1,37 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto.biblioteca;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
-/**
- *
- * @author Joseph Salas
- */
 public class Libro {
-
-    public Libro() {
+     private String Nombre;
+    private String Autor;
+    private String Serie;
+    private Calendar Fecha_Entregado;
+    private Calendar Fecha_Devolucion;
+	
+	
+    //Funcion para obtener la fecha como un str
+    private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date()); //Obtiene la fecha actual
+        return dateFormat.format(c);
     }
     
-        private String string;
-
-    /**
-     * Get the value of string
-     *
-     * @return the value of string
-     */
-    public String getString() {
-        return string;
+    //Getters y Setters
+    
+    public String getNombre() {
+        return Nombre;
+    }
+    
+    
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+    
+    
+    public String getAutor() {
+        return Autor;
+    }
+    
+    
+    public void setAutor(String autor) {
+        Autor = autor;
+    }
+    
+    
+    public String getSerie() {
+        return Serie;
+    }
+    
+    
+    public void setSerie(String serie) {
+        Serie = serie;
+    }
+    
+    
+    public Calendar getFecha_Entregado() {
+        return Fecha_Entregado;
+    }
+    
+    
+    public void setFecha_Entregado(Calendar fecha_Entregado) {
+        Fecha_Entregado = fecha_Entregado;
+    }
+    
+    
+    public Calendar getFecha_Devolucion() {
+        return Fecha_Devolucion;
     }
 
-    /**
-     * Set the value of string
-     *
-     * @param string new value of string
-     */
-    public void setString(String string) {
-        this.string = string;
+
+    public void setFecha_Devolucion(Calendar fecha_Devolucion) {
+	Fecha_Devolucion = fecha_Devolucion;
     }
 
     
