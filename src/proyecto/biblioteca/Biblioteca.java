ded201748991dd704225a2c7b0411b7fdbeab322
Joseph Salas Solano <josephsalas1112@gmail.com>
2018-03-04 11:54:07 -0600

    Interfaz proyecto biblioteca

---
+++ src/proyecto/biblioteca/Biblioteca.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.biblioteca;
+
+/**
+ *
+ * @author Joseph Salas
+ */
+public class Biblioteca {
+
+    public Biblioteca() {
+    }
+    
+    private String string;
+
+    /**
+     * Get the value of string
+     *
+     * @return the value of string
+     */
+    public String getString() {
+        return string;
+    }
+
+    /**
+     * Set the value of string
+     *
+     * @param string new value of string
+     */
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    
+    
+}
---
+++ src/proyecto/biblioteca/ProyectoBiblioteca.java
@@ -0,0 +1,15 @@
+
+package proyecto.biblioteca;
+import Windows.MainMenu;
+
+
+public class ProyectoBiblioteca {
+
+    public static void main(String[] args) {
+        
+        MainMenu mainWindow = new MainMenu();
+        mainWindow.setVisible(true);
+
+    }
+    
+}
